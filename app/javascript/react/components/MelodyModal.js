1caf482f6d51c290ef07a77861f711119f9461e6
Aidan Harris <Ahharris117@gmail.com>
2020-05-13 16:27:15 -0400

    added modals to show page, formatted showpage chart

---
+++ app/javascript/react/components/MelodyModal.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+const MelodyModal = props => {
+
+  const hide = () => {
+    props.hideModal("melody")
+  }
+
+  let melodyNotes;
+
+  if (props.melody) {
+    melodyNotes = props.melody.join(' ')
+  }
+
+  return(
+    <Modal show={props.show} size="lg" onHide={hide}>
+      <Modal.Header>
+        <Modal.Title>Melody Notes:</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <div>{melodyNotes}</div>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="primary" onClick={hide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default MelodyModal
